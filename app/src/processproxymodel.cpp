beaed426e73674fc92077f374f86fef81c6687a9
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-12-17 23:27:31 -0500

    Fix indentation.

--- app/src/processproxymodel.cpp
+++ app/src/processproxymodel.cpp
@@ -44,55 +44,55 @@ QVariant ProcessProxyModel::data(const QModelIndex &index, int role) const
         return QVariant();
     }
     else if (role == Qt::TextAlignmentRole)
+    {
+        switch(index.column())
         {
-            switch(index.column())
-            {
-            case StatsCore::ProcessField::Name:
-                return static_cast<int>(Qt::AlignLeft) | static_cast<int>(Qt::AlignVCenter);
-            case StatsCore::ProcessField::PID:
-                return static_cast<int>(Qt::AlignHCenter) | static_cast<int>(Qt::AlignVCenter);
-            default:
-                return static_cast<int>(Qt::AlignRight) | static_cast<int>(Qt::AlignVCenter);
-            }
+        case StatsCore::ProcessField::Name:
+            return static_cast<int>(Qt::AlignLeft) | static_cast<int>(Qt::AlignVCenter);
+        case StatsCore::ProcessField::PID:
+            return static_cast<int>(Qt::AlignHCenter) | static_cast<int>(Qt::AlignVCenter);
+        default:
+            return static_cast<int>(Qt::AlignRight) | static_cast<int>(Qt::AlignVCenter);
         }
+    }
     return unmodifiedData;
 }
 
 QVariant ProcessProxyModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
     if(orientation == Qt::Horizontal)
+    {
+        if (role == Qt::DisplayRole)
         {
-            if (role == Qt::DisplayRole)
+            switch(section)
             {
-                switch(section)
-                {
-                case StatsCore::ProcessField::Name:
-                    return QString("Process Name");
-                case StatsCore::ProcessField::PID:
-                    return QString("PID");
-                case StatsCore::ProcessField::CPU:
-                    return QString("CPU");
-                case StatsCore::ProcessField::Memory:
-                    return QString("Memory");
-                case StatsCore::ProcessField::Disk:
-                    return QString("Disk");
-                case StatsCore::ProcessField::Network:
-                    return QString("Network");
-                default:
-                    return QVariant();
-                }
-            }
-            else if (role == Qt::TextAlignmentRole)
-            {
-                if(section == 0)
-                    return static_cast<int>(Qt::AlignBottom) | static_cast<int>(Qt::AlignLeft);
-                else if(section == 1)
-                    return static_cast<int>(Qt::AlignBottom) | static_cast<int>(Qt::AlignHCenter);
-                else
-                    return static_cast<int>(Qt::AlignBottom) | static_cast<int>(Qt::AlignRight);
+            case StatsCore::ProcessField::Name:
+                return QString("Process Name");
+            case StatsCore::ProcessField::PID:
+                return QString("PID");
+            case StatsCore::ProcessField::CPU:
+                return QString("CPU");
+            case StatsCore::ProcessField::Memory:
+                return QString("Memory");
+            case StatsCore::ProcessField::Disk:
+                return QString("Disk");
+            case StatsCore::ProcessField::Network:
+                return QString("Network");
+            default:
+                return QVariant();
             }
         }
-        return QVariant();
+        else if (role == Qt::TextAlignmentRole)
+        {
+            if(section == 0)
+                return static_cast<int>(Qt::AlignBottom) | static_cast<int>(Qt::AlignLeft);
+            else if(section == 1)
+                return static_cast<int>(Qt::AlignBottom) | static_cast<int>(Qt::AlignHCenter);
+            else
+                return static_cast<int>(Qt::AlignBottom) | static_cast<int>(Qt::AlignRight);
+        }
+    }
+    return QVariant();
 }
 
 ProcessProxyModel::~ProcessProxyModel()
