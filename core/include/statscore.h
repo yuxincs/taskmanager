93441a51b12116d704ebaea250c23e8ea540717c
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-12-18 17:55:11 -0500

    Add SystemField enums.

--- core/include/statscore.h
+++ core/include/statscore.h
@@ -8,6 +8,8 @@ class StatsCore : public QObject
     Q_OBJECT
 public:
     enum ProcessField { Name = 0, PID, CPU, Memory, Disk, Network };
+    enum SystemField { CPUName = 0, Utilization, CPUSpeed, Processes, Temperature, UpTime, MaxSpeed, Cores, LogicalProcessors,
+                       UsedMemory, AvailableMemory, CachedMemory, ReservedMemory, MemorySpeed, MemorySockets, TotalSystemProperties};
     StatsCore() = delete;
     virtual ~StatsCore() { }
     virtual void setRefreshRate(int msec) = 0;
