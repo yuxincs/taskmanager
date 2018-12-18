bcd28480b71effd65876d0f0db98eb2f3447e553
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-12-18 15:07:47 -0500

    Make StatsCore an abstract class.
    Hide the implementations and OS-specific optimizations into subclasses.

--- core/src/statscore.cpp
+++ core/src/statscore.cpp
@@ -1,37 +1,7 @@
 #include "statscore.h"
+#include "genericstatscore.h"
 #include "macstatscore.h"
 #include "linuxstatscore.h"
-
-StatsCore::StatsCore(int msec, QObject *parent)
-    :QObject(parent)
-{
-    // connect timer to update functions
-    connect(&this->refreshTimer_, &QTimer::timeout, this, &StatsCore::updateProcesses);
-
-    // create a in-memory sqlite database to store all information
-    this->database_ = QSqlDatabase::addDatabase("QSQLITE");
-    this->database_.setDatabaseName(":memory:");
-    this->database_.open();
-    // create process info table
-    this->database_.exec("CREATE TABLE `process` (\
-                         `name`	TEXT,\
-                         `pid`	INTEGER UNIQUE,\
-                         `cpu`	REAL,\
-                         `memory`	INTEGER,\
-                         `disk`	INTEGER,\
-                         `network`	INTEGER,\
-                         PRIMARY KEY(`pid`)\
-                     );");
-    // create and set a QSqlTableModel for GUI
-    this->processModel_ = new QSqlTableModel(this, this->database_);
-    this->processModel_->setTable("process");
-
-    // initial update
-    this->updateProcesses();
-
-    // start timer
-    this->refreshTimer_.start(msec);
-}
 
 StatsCore *StatsCore::createCore(int msec, QObject *parent)
 {
@@ -39,94 +9,7 @@ StatsCore *StatsCore::createCore(int msec, QObject *parent)
     return new LinuxStatsCore(msec, parent);
     #elif defined(__APPLE__)
     return new MacStatsCore(msec, parent);
+    #else
+    return new GenericStatsCore(msec, parent);
     #endif
-}
-
-void StatsCore::setRefreshRate(int msec)
-{
-    this->refreshTimer_.setInterval(msec);
-}
-
-StatsCore::~StatsCore()
-{
-    emit this->shuttingDown();
-    delete this->processModel_;
-}
-
-QSqlTableModel *StatsCore::processModel()
-{
-    return this->processModel_;
-}
-
-void StatsCore::updateProcesses()
-{
-    // update the processes based on a `ps` implementation
-    // initialize the process object and connect to a function that updates the database
-    static QProcess *process = nullptr;
-    if(process == nullptr)
-    {
-        process = new QProcess();
-        connect(process, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished), [=] {
-            QString psOutput = process->readAllStandardOutput();
-            QStringList processList = psOutput.split('\n');
-            // remove the first line (title) and last line (blank line)
-            processList.removeFirst();
-            processList.removeLast();
-
-            // begin transaction
-            this->database_.transaction();
-            QSqlQuery query(this->database_);
-
-            // clear process table data
-            query.exec("DELETE from process;");
-
-            for(const QString &process: processList)
-            {
-                quint64 pid = process.section(' ', 0, 0, QString::SectionSkipEmpty).toULongLong();
-                double cpuPercent = process.section(' ', 1, 1, QString::SectionSkipEmpty).toDouble();
-                double memory = process.section(' ', 2, 2, QString::SectionSkipEmpty).toDouble();
-                QString name = process.section(' ', 3, -1, QString::SectionSkipEmpty);
-                name = name.mid(name.lastIndexOf('/') + 1);
-                query.prepare("INSERT INTO process (pid, name, cpu, memory, disk, network)"
-                              "VALUES (:pid, :name, :cpu, :memory, :disk, :network)");
-                query.bindValue(":pid", pid);
-                query.bindValue(":name", name);
-                query.bindValue(":cpu", cpuPercent);
-                query.bindValue(":memory", memory);
-                query.bindValue(":disk", 0);
-                query.bindValue(":network", 0);
-                query.exec();
-            }
-            this->database_.commit();
-            qDebug() << "Updated " << processList.size() << "processes.";
-            this->processModel_->select();
-        });
-        connect(this, &StatsCore::shuttingDown, [=]{
-            disconnect(process, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
-                    nullptr, nullptr);
-            process->kill();
-            process->waitForFinished();
-            delete process;
-        });
-    }
-    // if the last update is still running
-    if (process->state() == QProcess::Running)
-        return;
-    // else start updating
-    else
-        process->start("ps axo pid,%cpu,rss,comm");
-}
-
-void StatsCore::killProcess(quint64 pid)
-{
-    // a command based implementation
-    static QProcess *process = new QProcess();
-    connect(this, &StatsCore::shuttingDown, [=] {
-       disconnect(process, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
-               nullptr, nullptr);
-       process->waitForFinished();
-       delete process;
-    });
-    process->start("kill", {"-s", "KILL", QString::number(pid)});
-    qDebug() << "Killed " << pid;
 }
