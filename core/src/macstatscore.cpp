2c6821f8844255ea6df586a5e72c090812935c4c
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-12-20 10:23:06 -0500

    Implement getting cores and logical cores information.

--- core/src/macstatscore.cpp
+++ core/src/macstatscore.cpp
@@ -26,10 +26,16 @@ void MacStatsCore::gatherStaticInformation()
     qDebug() << "Gathering static information";
     char stringBuffer[1024];
     size_t stringSize = sizeof(stringBuffer);
+    int intBuffer = -1;
+    size_t intSize = sizeof(intBuffer);
     sysctlbyname("machdep.cpu.brand_string", &stringBuffer, &stringSize, nullptr, 0);
     QStringList cpuNameList = QString::fromLocal8Bit(stringBuffer, static_cast<int>(stringSize)).split('@');
     this->staticSystemInfo_[StatsCore::StaticSystemField::CPUName] = cpuNameList[0].remove("CPU").trimmed();
     this->staticSystemInfo_[StatsCore::StaticSystemField::MaxSpeed] = cpuNameList[1].trimmed();
+    sysctlbyname("machdep.cpu.core_count", &intBuffer, &intSize, nullptr, 0);
+    this->staticSystemInfo_[StatsCore::StaticSystemField::Cores] = QString::number(intBuffer);
+    sysctlbyname("machdep.cpu.thread_count", &intBuffer, &intSize, nullptr, 0);
+    this->staticSystemInfo_[StatsCore::StaticSystemField::LogicalProcessors] = QString::number(intBuffer);
     return;
 }
 
