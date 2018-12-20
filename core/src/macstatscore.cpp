65ecc2bf09b1862ff9930dfc3e36093c3452369c
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-12-20 00:25:44 -0500

    Implement CPUName and MaxSpeed.

--- core/src/macstatscore.cpp
+++ core/src/macstatscore.cpp
@@ -1,5 +1,7 @@
 #if defined (__APPLE__)
 #include "macstatscore.h"
+#include <sys/types.h>
+#include <sys/sysctl.h>
 #include <QDebug>
 
 MacStatsCore::MacStatsCore(int msec, QObject *parent)
@@ -22,6 +24,12 @@ void MacStatsCore::updateProcesses()
 void MacStatsCore::gatherStaticInformation()
 {
     qDebug() << "Gathering static information";
+    char stringBuffer[1024];
+    size_t stringSize = sizeof(stringBuffer);
+    sysctlbyname("machdep.cpu.brand_string", &stringBuffer, &stringSize, nullptr, 0);
+    QStringList cpuNameList = QString::fromLocal8Bit(stringBuffer, static_cast<int>(stringSize)).split('@');
+    this->staticSystemInfo_[StatsCore::StaticSystemField::CPUName] = cpuNameList[0].remove("CPU").trimmed();
+    this->staticSystemInfo_[StatsCore::StaticSystemField::MaxSpeed] = cpuNameList[1].trimmed();
     return;
 }
 
