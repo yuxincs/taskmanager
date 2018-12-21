902a265924e34dd54a3f85d8134aebb17a2d32d5
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-12-20 23:48:58 -0500

    Implement temperature update.

--- core/src/linuxstatscore.cpp
+++ core/src/linuxstatscore.cpp
@@ -1,5 +1,8 @@
 #if defined(unix) || defined(__unix__) || defined(__unix)
 #include "linuxstatscore.h"
+#include <QFile>
+#include <QDebug>
+#include <QStringListModel>
 
 LinuxStatsCore::LinuxStatsCore(int msec, QObject *parent)
     :GenericStatsCore(msec, parent)
@@ -19,11 +22,19 @@ void LinuxStatsCore::updateProcesses()
 
 void LinuxStatsCore::gatherStaticInformation()
 {
+    qDebug() << "Gathering linux static information";
     return;
 }
 
 void LinuxStatsCore::updateSystemInfo()
 {
+    qDebug() << "Linux update system information";
+    // update temperature
+    QFile inputFile("/sys/class/hwmon/hwmon0/temp1_input");
+    if(!inputFile.open(QIODevice::ReadOnly))
+        return;
+    this->systemModel_->setData(this->systemModel_->index(StatsCore::DynamicSystemField::Temperature), QString(inputFile.readAll()).toInt() / 1000.0);
+
     return;
 }
 
