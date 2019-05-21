4a36aa3f82c3423bc8f56d389bfdbea05d30e247
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-05-21 00:09:11 -0400

    Add react echarts.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,128 @@
-import { exists } from 'fs';
 import React, { Component } from 'react';
-import { Button, Modal } from 'antd';
+import { Tabs, Icon, Table } from 'antd';
+import ReactEcharts from 'echarts-for-react';
 import './App.css';
 
+function copy(o) {
+  let output, v, key;
+  output = Array.isArray(o) ? [] : {};
+  for (key in o) {
+    v = o[key];
+    output[key] = (typeof v === "object") ? copy(v) : v;
+  }
+  return output;
+}
+
+const columns = [
+  {
+    title: 'Process',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'PID',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'CPU',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Memory',
+    dataIndex: 'memory',
+    key: 'memory',
+  },
+  {
+    title: 'Disk',
+    dataIndex: 'disk',
+    key: 'disk',
+  },
+];
+
+
 class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      option: {
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          splitLine: {
+            show: false
+          },
+        },
+        yAxis: {
+          type: 'value',
+          boundaryGap: [0, '100%'],
+          splitLine: {
+            show: false
+          }
+        },
+        series: [{
+          data: Array.from({length: 60}, () => Math.floor(Math.random() * 100)),
+          type: 'line',
+          areaStyle: {},
+          showSymbol: false,
+          hoverAnimation: false,
+        }]
+      }
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      console.log('updating');
+      var option = copy(this.state.option);
+      option.series[0].data.shift();
+      option.series[0].data.push(Math.floor(Math.random() * 100));
+      this.setState({
+        option: option
+      });
+    }, 1000);
+  }
+
   render() {
+    console.log(this.state.option);
     return (
-        <div>
-            <Button type="primary" onClick={() => { exists('/proc', ex => { console.log(ex); }); }}>Button</Button>
-            <Button type="primary">Button</Button>
-        </div>
+      <Tabs defaultActiveKey="1" size="small">
+        <Tabs.TabPane
+          tab={
+            <span>
+              <Icon type="switcher" />
+              Processes
+            </span>
+          }
+          key="1">
+          <Table dataSource={[]} columns={columns} />
+        </Tabs.TabPane>
+        <Tabs.TabPane
+          tab={
+            <span>
+              <Icon type="rocket" />
+              Performance
+            </span>
+          }
+          key="2">
+          <Tabs defaultActiveKey="1" tabPosition="left" size="large">
+            <Tabs.TabPane tab="CPU" key="1">
+              <span>
+               <ReactEcharts
+                 ref='echarts_react'
+                 option={this.state.option} />
+              </span>
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Memory" key="2">
+              <ReactEcharts
+                ref="echartsInstance"
+                option={this.state.option}>
+              </ReactEcharts>
+            </Tabs.TabPane>
+          </Tabs>
+        </Tabs.TabPane>
+      </Tabs>
     );
   }
 }
