8985c948c6c684ebea9157fe36a35cc4f0a1ea44
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-05-28 00:51:09 -0400

    Rename the internal state name.

--- src/reducers/processes.js
+++ src/reducers/processes.js
@@ -1,16 +1,24 @@
 import { UPDATE_PROCESS_INFO } from "../constants/action-types";
 
 let initialState = {
-  processes: []
+  data: {
+    all: 0,
+    blocked: 0,
+    running: 0,
+    sleeping: 0,
+    unknown: 0,
+    list: []
+  }
 };
 
 export default function processes(state = initialState, action) {
   switch(action.type) {
     case UPDATE_PROCESS_INFO: {
       return Object.assign({}, state, {
-        processes: action.processes
+        data: action.processes
       });
     }
+    default:
+      return state;
   }
-  return state;
 }
