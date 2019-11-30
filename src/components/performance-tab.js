3071621baedf7d2545219bac68f01e4cdd46b207
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-11-29 21:40:20 -0500

    Update performance tab.
    Update style sheets and insert actual data into the chart.

--- src/components/performance-tab.js
+++ src/components/performance-tab.js
@@ -2,76 +2,80 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Tabs } from "antd";
 import ReactEcharts from "echarts-for-react";
+import './performance-tab.css';
 
-function copy(o) {
-  let output, v, key;
-  output = Array.isArray(o) ? [] : {};
-  for (key in o) {
-    v = o[key];
-    output[key] = (typeof v === "object") ? copy(v) : v;
-  }
-  return output;
-}
 
 export default class PerformanceTab extends React.Component {
   static propTypes = {
     className: PropTypes.string,
-    cpuLoad: PropTypes.number,
-    memLoad: PropTypes.number
+    cpuLoadHistory: PropTypes.arrayOf(PropTypes.number),
+    memLoadHistory: PropTypes.arrayOf(PropTypes.number)
   };
 
   static defaultProps = {
-    cpuLoad: 0,
-    memLoad: 0
+    cpuLoadHistory: Array.from({length: 60}, () => 0),
+    memLoadHistory: Array.from({length: 60}, () => 0)
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      option: {
-        xAxis: {
-          type: 'category',
-          boundaryGap: false,
-          splitLine: {
-            show: false
-          },
-        },
-        yAxis: {
-          type: 'value',
-          boundaryGap: [0, '100%'],
-          splitLine: {
-            show: false
-          }
-        },
-        series: [{
-          data: Array.from({length: 60}, () => Math.floor(Math.random() * 100)),
-          type: 'line',
-          areaStyle: {},
-          showSymbol: false,
-          hoverAnimation: false,
-          animation: false
-        }]
-      }
-    };
-  }
-
-  componentDidMount() {
-    setInterval(() => {
-      var option = copy(this.state.option);
-      option.series[0].data.shift();
-      option.series[0].data.push(Math.floor(Math.random() * 100));
-      this.setState({
-        option: option
-      });
-    }, 1000);
-  }
-
   render() {
-    let cpuChart = <ReactEcharts ref='echarts_react' option={this.state.option} />;
-    let memoryChart = <ReactEcharts ref='echarts_react' option={this.state.option} />;
-    return <Tabs defaultActiveKey="1" tabPosition="left" size="large">
+    let option1 = {
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        splitLine: {
+          show: false
+        },
+      },
+      yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%'],
+        splitLine: {
+          show: false
+        }
+      },
+      series: [{
+        data: this.props.cpuLoadHistory,
+        type: 'line',
+        areaStyle: {},
+        showSymbol: false,
+        hoverAnimation: false,
+        animation: false
+      }]
+    };
+    let option2 = {
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        splitLine: {
+          show: false
+        },
+      },
+      yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%'],
+        splitLine: {
+          show: false
+        }
+      },
+      series: [{
+        data: this.props.memLoadHistory,
+        type: 'line',
+        areaStyle: {},
+        showSymbol: false,
+        hoverAnimation: false,
+        animation: false
+      }]
+    };
+    let cpuChart = <ReactEcharts className="chart" ref='echarts_react' option={option1} key="1" />;
+    let memoryChart = <ReactEcharts className="chart" ref='echarts_react' option={option2} key="2"/>;
+    return <Tabs
+      className="performance-tab"
+      defaultActiveKey="1"
+      tabPosition="left"
+      size="large"
+    >
       <Tabs.TabPane tab="CPU" key="1"> {cpuChart} </Tabs.TabPane>
-      <Tabs.TabPane tab="Memory" key="2"> {memoryChart}</Tabs.TabPane>
+      <Tabs.TabPane tab="Memory" key="2"> {memoryChart} </Tabs.TabPane>
     </Tabs>
   }
 }
