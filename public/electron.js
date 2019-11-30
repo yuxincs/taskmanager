d4a98869fd5148c53da362e91d3b04beb0466cde
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-11-30 00:42:57 -0500

    Change default window size.

--- public/electron.js
+++ public/electron.js
@@ -8,7 +8,7 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 500, height: 660, webPreferences: { nodeIntegration: true }});
+    mainWindow = new BrowserWindow({width: 650, height: 700, webPreferences: { nodeIntegration: true }});
     mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
     if (isDev) {
         // Open the DevTools.
