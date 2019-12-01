9c7083e16964b66338acdf93acb992a15ae38907
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-11-30 22:54:02 -0500

    Revert "Create tree structure of process list."
    This reverts commit b4906f61e680fc1b9c6d4dfe097f498dcda8a86a.

--- src/actions/process-tab.js
+++ src/actions/process-tab.js
@@ -29,33 +29,6 @@ export function killProcess(pid) {
 export function requestProcessInfo() {
   return async (dispatch) => {
     const [procs, curLoad, memory] = await Promise.all([processes(), currentLoad(), mem()]);
-
-    // sort the process list by its parent pid for later use
-    procs.list = procs.list.sort((a, b) => a.parentPid - b.parentPid);
-
-    // create an empty root node with pid 0
-    let root = {children: []};
-
-    // process map from pid to node
-    let processMap = { 0 : root };
-
-    for(const process of procs.list) {
-      // move the children of init process (pid 1) directly to the root node for better readability
-      // otherwise the tree would look like a single process -> all other processes
-      if(process.pid === 1) {
-        processMap[process.pid] = root;
-      }
-      else {
-        processMap[process.pid] = process;
-      }
-      if(typeof processMap[process.parentPid].children === 'undefined') {
-        processMap[process.parentPid].children = [];
-      }
-      processMap[process.parentPid].children.push(process);
-    }
-
-    // flatten the pid 1 init process for better readability
-    procs.list = root.children;
     dispatch(updateProcessInfo(procs, curLoad, memory));
   }
 }
