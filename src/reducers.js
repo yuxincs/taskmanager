92b07a5d06990cd32ad11b1da81f7a2837c94a68
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-12-09 20:05:38 -0500

    Fix combineReducers function call.

--- src/reducers.js
+++ src/reducers.js
@@ -5,10 +5,10 @@ import cpu from "./reducers/cpu";
 import { combineReducers } from "redux";
 
 const reducer = combineReducers({
-  process: process(),
-  cpu: cpu(),
-  memory: memory(),
-  disk: disk()
+  process: process,
+  cpu: cpu,
+  memory: memory,
+  disk: disk
 });
 
 export default reducer;
