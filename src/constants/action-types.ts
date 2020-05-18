3643969307bdd3888eb571a29be3a970521542e9
Yuxin Wang <yuxinwang.dev@gmail.com>
2020-05-17 21:28:45 -0400

    Add type annotations to action-types.

---
+++ src/constants/action-types.ts
@@ -0,0 +1,9 @@
+export const UPDATE_GENERAL_INFO: string = 'UPDATE_GENERAL_INFO';
+export const UPDATE_PROCESS_INFO: string = 'UPDATE_PROCESS_INFO';
+export const UPDATE_CPU_INFO: string = 'UPDATE_CPU_INFO';
+export const UPDATE_CPU_LOAD: string = 'UPDATE_CPU_LOAD';
+export const UPDATE_CPU_CURRENT_SPEED: string = 'UPDATE_CPU_CURRENT_SPEED';
+export const UPDATE_MEMORY_INFO: string = 'UPDATE_MEMORY_INFO';
+export const UPDATE_MEMORY_LOAD: string = 'UPDATE_MEMORY_LOAD';
+export const UPDATE_DISK_LOAD: string = 'UPDATE_DISK_LOAD';
+export const PROCESS_KILLED: string = 'PROCESS_KILLED';
