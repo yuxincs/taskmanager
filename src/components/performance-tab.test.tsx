c5af1a4256dbdfe35e3aaa72338dba7e71b8572d
Yuxin Wang <yuxinwang.dev@gmail.com>
2020-05-18 23:12:15 -0400

    Replace the request* function with actual code.

--- src/components/performance-tab.test.tsx
+++ src/components/performance-tab.test.tsx
@@ -5,7 +5,12 @@ import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import reducer from "../reducers";
 import thunkMiddleware from "redux-thunk";
-import {requestDynamicInfo, requestStaticInfo} from "../index";
+import {cpu, cpuCurrentspeed, cpuFlags, currentLoad, disksIO, mem, memLayout, processes, time} from "systeminformation";
+import {updateCPUCurrentSpeed, updateCPUInfo, updateCPULoad} from "../actions/cpu";
+import {updateMemoryInfo, updateMemoryLoad} from "../actions/memory";
+import {updateProcessInfo} from "../actions/process";
+import {updateDiskLoad} from "../actions/disk";
+import {UpdateUpTime} from "../actions/general";
 
 const store = createStore(
   reducer,
@@ -21,8 +26,17 @@ it('renders without crashing', () => {
 
 it('renders with history data', () => {
   const div = document.createElement('div');
-  requestStaticInfo();
-  requestDynamicInfo();
+  // request static and dynamic information once for data to be populated
+  Promise.all([cpu(), cpuFlags()]).then(
+    ([info, flags]) => store.dispatch(updateCPUInfo({...info, flags: flags}))
+  );
+  memLayout().then((info) => store.dispatch(updateMemoryInfo(info)));
+  currentLoad().then((load) => store.dispatch(updateCPULoad(load)));
+  cpuCurrentspeed().then((speed) => store.dispatch(updateCPUCurrentSpeed(speed.avg)));
+  mem().then((load) => store.dispatch(updateMemoryLoad(load)));
+  processes().then((procs) => store.dispatch(updateProcessInfo(procs.list)));
+  disksIO().then((load) => store.dispatch(updateDiskLoad(load)));
+  store.dispatch(UpdateUpTime(parseInt(time().uptime)));
   ReactDOM.render(<Provider store={store}><PerformanceTab /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
