f7f7a63da0396e7c5a8ac39c5e5396e28a7adef9
Yuxin Wang <yuxinwang.dev@gmail.com>
2020-05-18 22:33:35 -0400

    Remove constants folder since it is no longer needed.

--- src/constants/action-types.ts
+++
@@ -1,6 +0,0 @@
-export const UPDATE_GENERAL_INFO: string = 'UPDATE_GENERAL_INFO';
-export const UPDATE_PROCESS_INFO: string = 'UPDATE_PROCESS_INFO';
-export const UPDATE_MEMORY_INFO: string = 'UPDATE_MEMORY_INFO';
-export const UPDATE_MEMORY_LOAD: string = 'UPDATE_MEMORY_LOAD';
-export const UPDATE_DISK_LOAD: string = 'UPDATE_DISK_LOAD';
-export const PROCESS_KILLED: string = 'PROCESS_KILLED';
