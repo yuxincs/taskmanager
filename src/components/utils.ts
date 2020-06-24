75c4a5f624e84168e3d9ad6e9d03f25d3c68d2aa
Yuxin Wang <yuxinwang.dev@gmail.com>
2020-06-24 15:11:45 -0400

    Use 1024 instead of 1000.

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -3,8 +3,8 @@ export const toMemoryString = (bytes: number, digits: number=2): [string, string
     return ['0', ''];
   }
   const units = ['B', 'KB', 'MB', 'GB', 'TB'];
-  const power = Math.floor(Math.log(bytes) / Math.log(1000));
-  return [`${(bytes / Math.pow(1000, power)).toFixed(digits)}`, units[Math.min(units.length - 1, power)]]
+  const power = Math.floor(Math.log(bytes) / Math.log(1024));
+  return [`${(bytes / Math.pow(1024, power)).toFixed(digits)}`, units[Math.min(units.length - 1, power)]]
 }
 
 export const isSelected = (rowIndex: number, {from, to}: {from: number, to: number}): boolean => {
