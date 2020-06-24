bfebb81da410b5115088701c9ad1c817ab264b95
Yuxin Wang <yuxinwang.dev@gmail.com>
2020-06-24 15:11:28 -0400

    Fix returning NaN when bytes === 0.

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,4 +1,7 @@
 export const toMemoryString = (bytes: number, digits: number=2): [string, string] => {
+  if(bytes === 0) {
+    return ['0', ''];
+  }
   const units = ['B', 'KB', 'MB', 'GB', 'TB'];
   const power = Math.floor(Math.log(bytes) / Math.log(1000));
   return [`${(bytes / Math.pow(1000, power)).toFixed(digits)}`, units[Math.min(units.length - 1, power)]]
