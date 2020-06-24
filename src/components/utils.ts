e57dae478ade8a9657657ee1b133b827c83bd481
Yuxin Wang <yuxinwang.dev@gmail.com>
2020-06-24 15:12:16 -0400

    Add toLevel utility function which updates corresponding level for cells.

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -7,6 +7,9 @@ export const toMemoryString = (bytes: number, digits: number=2): [string, string
   return [`${(bytes / Math.pow(1024, power)).toFixed(digits)}`, units[Math.min(units.length - 1, power)]]
 }
 
+export const toLevel = (percentage: number, maxLevel: number=8): number =>
+  Math.min(Math.ceil(maxLevel * percentage / 100), maxLevel);
+
 export const isSelected = (rowIndex: number, {from, to}: {from: number, to: number}): boolean => {
   if(from === -1 && to === -1)
     return false;
