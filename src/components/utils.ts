c309093f81d641892e77adaf5111248d84bc9b0a
Yuxin Wang <yuxinwang.dev@gmail.com>
2020-06-23 21:10:09 -0400

    Add utility functions for data display.

---
+++ src/components/utils.ts
@@ -0,0 +1,11 @@
+export const toMemoryString = (bytes: number, digits: number=2): [string, string] => {
+  const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+  const power = Math.floor(Math.log(bytes) / Math.log(1000));
+  return [`${(bytes / Math.pow(1000, power)).toFixed(digits)}`, units[Math.min(units.length - 1, power)]]
+}
+
+export const isSelected = (rowIndex: number, {from, to}: {from: number, to: number}): boolean => {
+  if(from === -1 && to === -1)
+    return false;
+  return rowIndex >= Math.min(from, to) && rowIndex <= Math.max(from, to)
+}
